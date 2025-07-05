93f3e85eb2dff2edfb44aac708425d8027ff30c9
Esanki Thambawita <esankilakvindee2000@gmail.com>
2025-07-05 20:57:36 +0530

    Add placeholder controller functions for auth, register, logout, and profile routes

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,25 @@
 import express from "express";
 import dotenv from "dotenv";
+import userRoutes from "./routes/userRoutes.js";
+import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
+
 dotenv.config();
 const port = process.env.PORT || 5000;
 
 const app = express();
+app.use(express.json());
+
+app.use("/api/users", userRoutes);
 
 app.get("/", (req, res) => res.send("Server is ready!"));
 
+app.use(notFound);
+app.use(errorHandler);
+
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+// - **POST /api/users** - Register a user
+// - **POST /api/users/auth** - Authenticate a user and get token
+// - **POST /api/users/logout** - Logout user and clear cookie
+// - **GET /api/users/profile** - Get user profile
+// - **PUT /api/users/profile** - Update profile
