5a0cfec439170a6517414b66a885279b15749e10
Esanki Thambawita <esankilakvindee2000@gmail.com>
2025-07-07 03:06:13 +0530

    Add frontend React app with Vite, Bootstrap setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { Container } from "react-bootstrap";
+import { Outlet } from "react-router-dom";
+import Header from "./components/Header";
+import HomeScreen from "./screens/HomeScreen";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Container className="my-2">
+        <Outlet />
+      </Container>
+    </>
+  );
+};
+
+export default App;
